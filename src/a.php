9d6d8f78d3a281e3e0992272a50096692ff7706e
smile <285387316@qq.com>
2019-11-18 10:39:23 +0800

    init

---
+++ src/a.php
@@ -0,0 +1,9 @@
+<?php
+namespace tp6rbac;
+class a
+{
+    public function index($a,$b)
+    {
+        return $a+$b;
+    }
+}
---
+++ test/b.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * This file is part of the mfcad.
+ *
+ * (c) zhang tianhao
+ *
+ * Date：2019/11/18 10:30
+ * 
+ */
+require_once '../vendor/autoload.php';
+
+use Tp6rbac\a;
+
+$aa=new a();
+echo $aa->index(1,2);
